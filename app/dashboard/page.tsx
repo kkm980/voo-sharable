570794c5c498f576c556a5eb5a484786b5163676
Tech Alchemy <krishna.kant@techalchemy.com>
2024-04-06 17:57:36 +0530

    added redux

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,35 @@
+"use client";
 import StickyScrollReveal from "@/components/dashboard/cardScroler/CardScroller";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { decrement, increment, reset, addName, setSupportingLangs, setLoading, setMultiLangInputListStore } from "@/redux/features/counterSlice";
 
 export default function Home() {
+  const count = useAppSelector((state: any) => state.counterReducer.count);
+  const name = useAppSelector((state: any) => state.counterReducer.name);
+  const supportingLangs = useAppSelector((state: any) => state.counterReducer.supportingLangs);
+  const loading = useAppSelector((state: any) => state.counterReducer.loading);
+  const multiLangInputListStore = useAppSelector((state: any) => state.counterReducer.multiLangInputListStore);
+  const dispatch = useAppDispatch();
   return (
     <main className="flex max-h-screen flex-col items-center justify-between p-24 overflow-hidden">
      <StickyScrollReveal/>
+     <div style={{ marginBottom: "4rem", textAlign: "center" }}>
+            <h4 style={{ marginBottom: 16 }}>{count}</h4>
+            <h4 style={{ marginBottom: 16 }}>{name}</h4>
+            <h4 style={{ marginBottom: 16 }}>{supportingLangs.length}..</h4>
+            <button onClick={() => dispatch(increment())}>increment</button>
+            <button
+              onClick={() => {
+                dispatch(decrement());
+                dispatch(addName("shyam"));
+                dispatch(setSupportingLangs("alpha"));
+              }}
+              style={{ marginInline: 16 }}
+            >
+              decrement
+            </button>
+            <button onClick={() => dispatch(reset())}>reset</button>
+          </div>
     </main>
   );
 }
